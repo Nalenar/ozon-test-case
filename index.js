c1fd44a6747c6a81fb2a49524168952841e57f14
Nikita Zinovyev <nikita.zinovyev.staff@gmail.com>
2026-01-16 14:49:24 +0300

    Added functionality for toggling animation

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const progressBar = document.querySelector(".progress-bar");
 const progressInput = document.getElementById("progress-value");
+const animateToggle = document.getElementById("animate-check");
 
 progressInput.addEventListener("input", function () {
   const currentInputValue = this.value;
   progressBar.setAttribute("style", `--progress: ${currentInputValue || 0}`);
+});
+
+animateToggle.addEventListener("change", function () {
+  progressBar.classList.toggle("animation-rotate");
 });
